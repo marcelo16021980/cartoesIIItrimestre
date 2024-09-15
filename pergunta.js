ee071096ce0c003c3cc82f526f5905d6cd42ac39
Jorge marcelo Costa <162852235+marcelo16021980@users.noreply.github.com>
2024-09-15 18:43:23 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,19 @@
+criaCartao(
+    'Programacao',
+    'O que e JavaScript?',
+    'O Javascript e uma linguagem de programacao');
+
+criaCartao(
+    'Geografia',
+    'Qual e a capital da Franca?',
+    'Paris');
+
+criaCartao(
+    'Programacao',
+    'O que e uma funcao?',
+    'Uma funcao e um bloco de codigo que executa uma tarefa');
+
+criaCartao(
+    'Historia',
+    'Qual foi o ano da formatura de Newton?',
+    '1666');
